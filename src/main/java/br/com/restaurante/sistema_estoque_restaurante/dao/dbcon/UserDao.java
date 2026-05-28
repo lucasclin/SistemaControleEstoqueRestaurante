a82848307314973a88b4bb3ea9cbb431c591f197
Magocho <159202100+Magocho@users.noreply.github.com>
2026-05-28 11:13:35 -0300

    Add files via upload

--- src/main/java/br/com/restaurante/sistema_estoque_restaurante/dao/dbcon/UserDao.java
+++ src/main/java/br/com/restaurante/sistema_estoque_restaurante/dao/dbcon/UserDao.java
@@ -7,53 +7,71 @@
  * PRIMARY KEY(id)
  * );
  */
-package br.com.restaurante.sistema_estoque_restaurante.dao.dbcon;
+package br.com.restaurante.sistema_estoque_restaurante.model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-import br.com.restaurante.sistema_estoque_restaurante.model.TipoCadastro;
-import br.com.restaurante.sistema_estoque_restaurante.model.User;
+import dbcon.AcessoSQLie;
+import model.User;
+import model.Enum.TipoCadastro;
 
 public class UserDao {
 
   /* Como economizamos espaço no db, optei por fazer um função que traduz do DB --> model */
-  private void formatarUser(User resultado, ResultSet saida) throws SQLException {
-    resultado.setId(Long.valueOf(saida.getInt("id")));
-    resultado.setNome(saida.getString("nome"));
-    resultado.setSenha(saida.getString("senha"));
-    resultado.setTipoCadastro(TipoCadastro.tipoCadastroInt(saida.getInt("tipoCadastro")));
-  }
+  private User formatarUser(ResultSet saida) throws SQLException {
+    User userAux = new User();
+    
+    userAux.setId(saida.getInt("id"));
+    userAux.setNome(saida.getString("nome"));
+    userAux.setSenha(saida.getString("senha"));
+    userAux.setTipoCadastro(TipoCadastro.tipoCadastroInt(saida.getInt("tipoCadastro")));
 
-  
+    return userAux;
+  }
 
   /* 
    * Func precisa: nome (usuário) e senha para bater retorna TRUE se sucesso e FALSE se contrário.
    *  FALTA MUDAR
    */
-  public boolean retornar(int id, User resultado) {
-    String sql = "SELECT * FROM user WHERE id = ?";
+  public boolean fazerLoginNome(String nome, String senha, User resultado) {
+    String sql = "SELECT * FROM cliente " +
+                " WHERE (email = ?);";
     Connection con = null;
-    PreparedStatement cmd;
-    ResultSet saida;
+    PreparedStatement cmd = null;
+    ResultSet saida = null;
+
     try {
-      con = AcessoSQLite.conectar();
+      con = AcessoPSQL.conectar();
       if (con == null) {
         return false;
       }
       con.setAutoCommit(false);
 
       cmd = con.prepareStatement(sql);
-      cmd.setInt(1, id);
+      cmd.setString(1, email);
+
       saida = cmd.executeQuery();
 
       /* 1 - Existe conta com tal email? */
       if (saida.next()) {
-        formatarUser(resultado, saida);
-        
-        return true;
+        /* Existe!
+         * Agora a senha bate? */
+        if(senha.equals(saida.getString("senhaHash"))) {
+          /* Sim! */
+          Cliente aux = this.formatarCliente(saida);
+          resultado.setCadastro(aux.getCadastro());
+          resultado.setCpf(aux.getCpf());
+          resultado.setTelefone(aux.getTelefone());
+          resultado.setNomeCompleto(aux.getNomeCompleto());
+          resultado.setId(aux.getId());
+          return true;
+        }
       }
       
       return false;
@@ -61,7 +79,7 @@ public class UserDao {
       System.out.println("ERRO" + deuRuim.getMessage());
       return false;
     } finally {
-      AcessoSQLite.desconectar(con);
+      AcessoPSQL.desconectar(con);
     }
   }
 }
