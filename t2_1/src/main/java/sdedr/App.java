9adec2a6cc2f387b6d39e16e88daec4873b17d1b
lucas cho <lucas.lupo2003@gmail.com>
2026-06-09 23:44:20 -0300

    Teste com tabelas

---
+++ t2_1/src/main/java/sdedr/App.java
@@ -0,0 +1,62 @@
+package sdedr;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/* JavaFX coisas:
+ * link legal: https://docs.oracle.com/javafx/2/get_started/hello_world.htm
+ * obs. É dinâmico! Whoa que moderno... */
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.text.Font;
+
+import sdedr.view.*;
+import sdedr.ctrl.CtrlCtrl;
+
+public class App extends Application {
+  /* Palco das cenas */
+  private Stage principalStage;
+
+  @Override
+  public void start(Stage stage) {
+    /* Vamos construir e manipular LoginView em seu .java Mas instaciamos ele aqui no App, como uma tela. */ 
+    this.principalStage = stage;
+    this.principalStage.setTitle("SDEDR - Sistema de estoque de restaurante");
+    /* A manipulação das telas ficarão no view */
+    Font.loadFont(getClass().getResourceAsStream("/fonts/BlexMonoNerdFont-Regular.ttf"), 12);
+    vezDeLoginView();
+  }
+
+  /* Se tiver a opção de terminar sessão chamar esse método */
+  public void vezDeLoginView() {
+    LoginView loginView = new LoginView(this);
+    Scene loginTela = new Scene(loginView, 300, 250);
+    principalStage.setScene(loginTela);
+    principalStage.show();
+  }
+
+  public void vezMenuTmpView(CtrlCtrl aux) {    
+    MenuTmpView menuTmpView = new MenuTmpView(this, aux);
+    Scene menuTmpTela = new Scene(menuTmpView, 450, 500);
+    principalStage.setScene(menuTmpTela);
+    principalStage.show();
+  }
+
+  /* Se quiser ir para Tabelas de Produto ou Receita informar tipo */
+  public void vezDeManterGeralView(int tipo, CtrlCtrl aux) {
+    ManterGeralView manterGeralView = new ManterGeralView(this, tipo, aux);
+    Scene manterGeralTela = new Scene(manterGeralView, 450, 500);
+    principalStage.setScene(manterGeralTela);
+    principalStage.show();
+  }
+
+  private void applyGlobalStyles(Scene scene) {
+    String cssCam = getClass().getResource("/css/uniqlo.css").toExternalForm();
+    scene.getStylesheets().add(cssCam);
+  }
+}
---
+++ t2_1/src/main/java/sdedr/ctrl/CtrlCtrl.java
@@ -0,0 +1,17 @@
+package sdedr.ctrl;
+
+import sdedr.ctrl.*;
+
+/* Class CtrlCtrl
+ * (obj.)
+ * > Manter Ctrl por longo prazo          [ok!]
+ * > Facilitar referência e arg nas views [?] 
+ */
+
+public class CtrlCtrl {
+  public final UserCtrl userCtrl;
+
+  public CtrlCtrl() {
+    this.userCtrl = new UserCtrl();
+  }
+}
