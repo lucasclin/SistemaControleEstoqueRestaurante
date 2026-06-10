c481054a19031afd68914830c5844fbde22b531d
Splist <icarohamonpmarchiori@gmail.com>
2026-06-09 23:10:06 -0300

    modified:   t2/src/main/java/sdedr/App.java

--- t2/src/main/java/sdedr/App.java
+++ t2/src/main/java/sdedr/App.java
@@ -1,17 +1,11 @@
 package sdedr;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 /* JavaFX coisas:
  * link legal: https://docs.oracle.com/javafx/2/get_started/hello_world.htm
  * obs. É dinâmico! Whoa que moderno... */
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import sdedr.view.LoginView;
@@ -30,7 +24,7 @@ public class App extends Application {
     /* Vamos construir e manipular LoginView em seu .java
      * Mas instaciamos ele aqui no App, como uma tela. */ 
     this.principalStage = stage;
-    this.principalStage.setTitle("SDEDR - Sistema de estoque de restaurante");
+    this.principalStage.setTitle("Sistema de estoque de restaurante");
     /* Aqui teremos um "ArrayList", ou melhor uma árvore chamada:
      * primaryStage que conterá:
      *  loginTela - manterGeralView - etc.
